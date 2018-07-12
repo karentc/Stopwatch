38073a5e05adea55fb25d0a7e2979e1278ba411e
Karen Collins <karentc@vt.edu>
2018-07-12 15:26:20 -0400

    Update stopwatch.js

--- stopwatch.js
+++ stopwatch.js
@@ -1,55 +1,30 @@
-// First things first, create some variables:
 var clock = 0;
-var running = 0;
+var running = false;
+var startBtn = document.getElementById("start");
+var output = document.getElementById("output");
 
-
-
-
-
-// Secondly, this function will operate my start/pause button:
 function start(){
-	if(running == 0){								
-		running = 1;
+	if(running === false){								
+		running = true;
 		count();
-		document.getElementById("start").innerHTML = "Pause";
+		startBtn.innerHTML = "Pause";
 	}else{		
-		running = 0;														
-		document.getElementById("start").innerHTML = "Resume";
+		running = false;														
+		startBtn.innerHTML = "Resume";
 	}
 }
-// Basically, when 'start' is clicked: if the clock is NOT running:
- // (a) the clock will begin to run
-// (b) the count function will be called, and 
-// (c) getelementbyID will change my button HTML to "pause"
 
-// If it IS running, then:
-// (a) running will be set to 0 (turned off), and
-// (b) getelementbyID will change my button HTML to "resume"
-
-
-
-
-
-// Ok so next, this function will operate my reset button:
 function reset(){
-			running = 0;
+			running = false;
 			clock = 0;
-			document.getElementById("start").innerHTML = "Start";
-			document.getElementById("output").innerHTML = "00:00:00";
+			startBtn.innerHTML = "Start";
+			output.innerHTML = "00:00:00";
 }
-// Basically, this (a) sets the clock back to zero, and tells it that its NOT running, and
-// (b) changes all my HTML back to its starting points
 
-
-
-
-
-// Lastly, this function will 'count'
 function count(){
-	if(running == 1){
+	if(running === true){
 		setTimeout(function(){
 			clock++;
-			// some variables to start the count 
 			var mins = Math.floor(clock/10/60);
 			var secs = Math.floor(clock/10);
 			var tenths = clock % 10;
@@ -60,14 +35,9 @@ function count(){
 				if(secs < 10){
 					secs = "0" + secs;
 				}
-			document.getElementById("output").innerHTML = mins + ":" + secs + ":" + "0" + tenths;
+			output.innerHTML = mins + ":" + secs + ":" + "0" + tenths;
 			count();
 		}, 100,);
 	}
 }
-// If its running, we call a funtion (setTimeout) which has two parameters:
-		// (a) the function called
-		// (b) how long to wait to call it (100 ms)
-// Then, (a) increment the timer
-		// (b) update the output 
-		// (c) call the increment function again
+
